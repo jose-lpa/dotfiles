2f35e6e5b756d5f3ba2937d052e723a8ac2aaea5
José L. Patiño <jose.lpa@gmail.com>
2016-11-28 14:56:40 +0000

    Fixed `docker_cleanup` shell command.

--- scripts/docker_cleanup.sh
+++ scripts/docker_cleanup.sh
@@ -3,4 +3,7 @@
 dcleanup() {
     docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
     docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
+    docker volume rm $(docker volume ls -qf dangling=true)
 }
+
+dcleanup
