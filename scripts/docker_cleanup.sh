2092892173f3e5715f4cfddceeef1948eba1dd9f
José L. Patiño <jose.lpa@gmail.com>
2019-11-04 09:40:41 +0000

    Improved `docker_cleanup.sh` script.

--- scripts/docker_cleanup.sh
+++ scripts/docker_cleanup.sh
@@ -1,9 +1,29 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-dcleanup() {
-    docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
-    docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
-    docker volume rm $(docker volume ls -qf dangling=true)
+clean_containers() {
+    containers=$(docker ps -q)
+    echo "Stopping any running containers..."
+    docker stop $containers
+    echo "Removing all containers..."
+    docker rm -v $containers 2>/dev/null
+    echo "Containers removed from system."
 }
 
-dcleanup
+clean_images() {
+    echo "Removing Docker images..."
+    docker rmi --force $(docker images -q 2>/dev/null) 2>/dev/null
+    echo "All Docker images removed from system."
+}
+
+clean_environments() {
+    echo "Removing leftover volumes..."
+    docker volume rm --force $(docker volume ls -q)
+    echo "Pruning leftover networks..."
+    docker network prune --force
+}
+
+echo "Cleaning up Docker environment..."
+clean_containers
+clean_images
+clean_environments
+echo "Done."
